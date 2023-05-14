d984cc4f6ec32f90ee1aaae9f9c8e31eb0799c77
xDestyn <omar.flores.cs@outlook.com>
2023-05-14 16:14:30 -0500

    Project start cleanup

---
+++ src/main/java/com/xdestyn/Customer.java
@@ -0,0 +1,78 @@
+package com.xdestyn;
+
+import java.util.Objects;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private String age;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id,
+                    String name,
+                    String email,
+                    String age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
